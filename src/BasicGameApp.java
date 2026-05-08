12b88211285d5bac4d028d263d78580ef8222ef3
MikeG <MikeGao2024@outlook.com>
2026-05-08 14:56:09 -0400

    .

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -1,6 +1,24 @@
 public class BasicGameApp {
-    public Card[] cards;
-    public Player player_1;
-    public Dealer dealer_1;
+    public Card[] deck;
+    public Player player1;
+    public Dealer dealer1;
     public boolean game_on;
+
+    public static void main(String[] args) {
+        BasicGameApp a = new BasicGameApp();
+    }
+
+    public BasicGameApp(){
+        System.out.println("Welcome to Blackjack!");
+        deck = new Card[52];
+
+        for(int b = 0; b < deck.length; b++){
+            deck[b] = new Card(10, b, "Clubs");
+            deck[b].printInfo();
+        }
+    }
+
+    public void printDeck(){
+
+    }
 }
--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,26 @@
 public class Card {
     public Card[] cards;
-    public int values;
+    public int value;
     public String name;
     public String suit;
+
+    //todo: make a constructor that takes in three parameters and
+    public Card(int pvalues, int pname, String psuit){
+        value = pvalues;
+        if(pname == 0) {
+            name = "Ace";
+            value = 11;
+        } else if(pname == 1){
+            name = "Two";
+            value = 2;
+        }
+        suit = psuit;
+    }
+
+    public void printInfo(){
+//        System.out.println(values);
+//        System.out.println(name);
+//        System.out.println(suit);
+        System.out.println("The value of " + name + " of " + suit + " is worth " + value + " points.");
+    }
 }
